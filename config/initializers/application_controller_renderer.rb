9bb61279c6bb235ec25b2b8ba1ae7db8bab57982
1988o6o4 <tm1988o6o4@gmail.com>
2021-03-21 17:34:09 +0000

    [Finish]6-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'da25352b761d4e294dfef720be36a63afbd5a742bc19bb5afe79afaa63681a1bc7a8ca50a19ecf47d7cbba8c49f5430470be7c65046ceb4e52bd8422371060fd'
